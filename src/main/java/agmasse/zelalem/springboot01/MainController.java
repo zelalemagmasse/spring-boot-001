0156e1567eb5398b9b61156b7df64a6fbe9d0518
zelalemagmasse <zelalem.agmasse@gmail.com>
2018-06-20 16:08:28 -0400

    first intial commit

---
+++ src/main/java/agmasse/zelalem/springboot01/MainController.java
@@ -0,0 +1,37 @@
+package agmasse.zelalem.springboot01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @RequestMapping("/")
+    public String mainCourse(){
+        return "index";
+    }
+    @RequestMapping("/breakfast")
+    public String breakfast(){
+        return "breakfast";
+    }
+    @RequestMapping("/dessert")
+    public String dessert(){
+        return "Dessert";
+    }
+    @RequestMapping("/gallery")
+    public String gallery(){
+        return "gallery";
+    }
+    @RequestMapping("/salad")
+    public String salad(){
+        return "salad";
+    }
+    @RequestMapping("/sandwich")
+    public String sandwich(){
+        return "sandwich";
+    }
+    @RequestMapping("/soup")
+    public String soup(){
+        return "soup";
+    }
+
+}
